077a269e6de7f722ad4a2b486e7e494d9258a85e
tsipporahc <tsipporahc@gmail.com>
2022-09-12 10:57:41 -0700

    name, job role selection complete

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,17 @@
-console.log('Test');
+
+// "Name" feild - When the page first loads, the first text field should have the focus state by default to prompt the user.
+
+document.getElementById('name').focus();
+
+// "Job Role" section
+
+//when user selects other,  then focus on 'other' text
+// hide/ grey out 'other' text (default state)
+
+document.getElementById('other-job-role').style.display = "none";
+
+
+// select element listens for changes to its state
+
+const jobRole = document.querySelector('select[id="title"]');
+
