75c7c2d151e41d24bacbd3f99852bc88095e8534
tsipporahc <tsipporahc@gmail.com>
2022-09-12 11:18:15 -0700

    job role event listener complete

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -5,13 +5,29 @@ document.getElementById('name').focus();
 
 // "Job Role" section
 
-//when user selects other,  then focus on 'other' text
+//when user selects other,  then focus on 'other' text input
 // hide/ grey out 'other' text (default state)
 
 document.getElementById('other-job-role').style.display = "none";
 
 
 // select element listens for changes to its state
+//job role - drop down selection menu
 
-const jobRole = document.querySelector('select[id="title"]');
+const jobRole = document.querySelector('select[id="title"]'); 
 
+jobRole.addEventListener('change', e => {
+    if (e.target.value === 'other') {
+        document.getElementById('other-job-role').style.display = 'block';
+    } else {
+        document.getElementById('other-job-role').style.display = "none";
+
+    }
+
+});
+
+// "T-shirt Info" section 
+
+//user choose design first then color option
+
+// disable the color select element
